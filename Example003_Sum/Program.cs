4538ea1e1522be7ac5d46e77e91c81746a25a505
oksanaverkh <oksana-tyryatkina@yandex.ru>
2022-09-30 00:17:07 +0300

    random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 51;
+﻿int numberA = new Random().Next(1,80); // 1 2 3 ... 79
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,80); // 1 2 3 ... 79
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
