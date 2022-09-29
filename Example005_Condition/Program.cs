0db4f113dab9250186a543a1d45be9ba566379a5
oksanaverkh <oksana-tyryatkina@yandex.ru>
2022-09-30 00:34:06 +0300

    Example005_Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "jason")
+{
+    Console.WriteLine("Heeeeey, Jason");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+
